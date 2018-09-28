9c5a234ef26c646db9b43cfcc7f62cd29f58f57d
GuillermoMorales <00011417@uca.edu.sv>
2018-09-28 15:07:24 -0600

    Continuando

--- Ejercicio9.js
+++ Ejercicio9.js
@@ -3,7 +3,13 @@ var mensaje;
 
 function adivinanza(numero)
 {
-    let array =[1,2,3,4,5];
+    let array=[];
+    for(let j=0;j<=20;j++)
+    {
+        array.push(Math.floor(Math.random() * 101));
+    }
+    console.log(array);
+    
     for(let i=0;i<=array.length-1;i++)
     {
         if(numero == array[i])
